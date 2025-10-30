3ba87eaaafa289b93a89adb9a7024567355d386b
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-10-30 16:43:00 -0700

    Update setup-client1.sh

--- setup-automation/setup-client1.sh
+++ setup-automation/setup-client1.sh
@@ -6,10 +6,10 @@
 # done
 
 #useradd rhel
-subscription-manager register --org=12451665 --activationkey=${ACTIVATION_KEY} --force
+#subscription-manager register --org=12451665 --activationkey=${ACTIVATION_KEY} --force
 
-echo -e "test\ntest" | passwd rhel
-mkdir -p /home/rhel/.ssh
-cp .ssh/id_rsa.pub /home/rhel/.ssh/
-cp .ssh/authorized_keys /home/rhel/.ssh/
-chown rhel:rhel -R /home/rhel/.ssh/
+#echo -e "test\ntest" | passwd rhel
+#mkdir -p /home/rhel/.ssh
+#cp .ssh/id_rsa.pub /home/rhel/.ssh/
+#cp .ssh/authorized_keys /home/rhel/.ssh/
+#chown rhel:rhel -R /home/rhel/.ssh/

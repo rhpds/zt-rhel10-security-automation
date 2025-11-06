3254da07f9a220dd335b31609838c41092ac8263
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-11-05 16:56:57 -0800

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -61,33 +61,3 @@ cat >  /var/www/html/index.html << EOF
 EOF
 
 sudo dnf install  rhel-system-roles -y
-
-
-#useradd rhel
-#echo -e "test\ntest" | passwd rhel
-#mkdir -p /home/rhel/.ssh
-#cp .ssh/id_rsa.pub /home/rhel/.ssh/
-#cp .ssh/authorized_keys /home/rhel/.ssh/
-#chown rhel:rhel -R /home/rhel/.ssh/
-
-#ssh-keyscan -H client1 >> ~/.ssh/known_hosts
-
-#set up tmux so it has to restart itself whenever the system reboots
-
-#step 1: make a script
-#tee ~/startup-tmux.sh << EOF
-#TMUX='' tmux new-session -d -s 'rhel-session' > /dev/null 2>&1
-#tmux set -g pane-border-status top
-#tmux setw -g pane-border-format ' #{pane_index} #{pane_current_command}'
-#tmux set -g mouse on
-#tmux set mouse on
-#EOF
-
-#step 2: make it executable
-#chmod +x ~/startup-tmux.sh
-#step 3: use cron to execute 
-#echo "@reboot ~/startup-tmux.sh" | crontab -
-
-#step 4: start tmux for the lab
-#~/startup-tmux.sh
-

a6a43582db4aece2cf8bc20be90a5202c6990879
parmstro <parmstro@redhat.com>
2026-03-09 19:38:40 -0400

    module3

---
+++ runtime-automation/03-httpd-permission/solve-rhel.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "Solved module called 03-httpd-permission" >> /tmp/progress.log
--- runtime-automation/03-untitled-challenge-1d4dyo/setup-rhel.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "Starting module called 03-untitled-challenge-1d4dyo" >> /tmp/progress.log
